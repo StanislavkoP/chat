cb295799c2452a5395106c7c52c5e450f8f7285f
StanislavkoP <stas8327634@gmail.com>
2019-02-20 09:08:36 +0200

    Added button component

---
+++ src/components/UI/Button/Button.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+Button.propTypes = {
+    className: PropTypes.string,
+    type: PropTypes.string,
+    disabled: PropTypes.bool,
+    clicked: PropTypes.func,
+    children: PropTypes.string,
+}
+
+function Button (props) {
+    const { className, type, disabled, clicked } = props;
+
+    return (
+        <button 
+        className={ `ui button ${ className }` } 
+        type={ type }
+        disabled={ disabled }
+        onClick={ clicked }
+        > 
+            { props.children } 
+        </button>
+    )
+}
+
+export default Button;
