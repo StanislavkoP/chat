d1166e18137ef12ea3ce50d40abfaa9d73ba2ee1
StanislavkoP <stas8327634@gmail.com>
2019-02-20 09:11:20 +0200

    Fixed route

--- src/App.js
+++ src/App.js
@@ -11,28 +11,27 @@ import Profile from './containers/Profile/Profile';
 import EditProfile from './containers/EditProfile/EditProfile';
 import Layout from './hoc/Layout';
 import LogOut from './containers/LogOut/LogOut';
+
 class App extends Component {
 
     render() {
-
-            return (
-                
-                    <BrowserRouter>
-                        <Layout>
-                            <Switch>
-                                <Route exact path="/" component={FrontPage}/>
-                                <PrivateRoute path="/chat" component={Chat}/>
-                                <PrivateRoute path="/profile" component={Profile}/>
-                                <PrivateRoute path="/editprofile" component={EditProfile}/>
-                                <Route path="/auth" component={Auth}/>
-                                <Route path="/auth" component={Auth}/>
-                                <Route path="/logout" component={LogOut}/>
-                                <Redirect to="/" />
-                            </Switch>
-                        </Layout>
-                    </BrowserRouter>
-                
-            );
+        return (
+            
+                <BrowserRouter>
+                    <Layout>
+                        <Switch>
+                            <Route exact path="/" component={FrontPage}/>
+                            <PrivateRoute path="/chat" component={Chat}/>
+                            <PrivateRoute path="/profile" component={Profile}/>
+                            <PrivateRoute path="/editprofile" component={EditProfile}/>
+                            <Route path="/auth" component={Auth}/>
+                            <Route path="/logout" component={LogOut}/>
+                            <Redirect to="/" />
+                        </Switch>
+                    </Layout>
+                </BrowserRouter>
+            
+        );
     }
 }
 
