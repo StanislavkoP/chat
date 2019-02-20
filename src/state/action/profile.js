85dc8e6041023a51e710c296ada8750dd924a769
StanislavkoP <stas8327634@gmail.com>
2019-02-20 09:03:29 +0200

    Added loading

--- src/state/action/profile.js
+++ src/state/action/profile.js
@@ -1,13 +1,22 @@
 import axios from '../../axiosDefault';
 import * as actionTypes from './actionTypes';
 
+const getDataCurrentProfileInit = (userData) => ({
+    type: actionTypes.GET_DATA_CURRENT_PROFILE_INIT,
+})
 
 const getDataCurrentProfileSuccess = (userData) => ({
     type: actionTypes.GET_DATA_CURRENT_PROFILE_SUCCESS,
     userData
+});
+
+const getDataCurrentProfileFailed = () => ({
+    type: actionTypes.GET_DATA_CURRENT_PROFILE_FAILED,
 })
 
 export const getDataCurrentProfile = dispatch => {
+    dispatch( getDataCurrentProfileInit() )
+
     axios({
         method: 'get',
         url: '/profile',
@@ -15,23 +24,32 @@ export const getDataCurrentProfile = dispatch => {
     })
     .then(response => {
         const userData = response.data.data;
-        console.log(userData);
+
         dispatch( getDataCurrentProfileSuccess(userData) )
 
     })
     .catch(err => {
         console.log(err);
+        dispatch( getDataCurrentProfileFailed() )
     })
 };
+
+const updateDataCurrentProfileInit = () => ({
+    type: actionTypes.UPDATE_DATA_CURRENT_PROFILE_INIT,
+});
 
 const updateDataCurrentProfileSuccess = newDataUser => ({
     type: actionTypes.UPDATE_DATA_CURRENT_PROFILE_SUCCESS,
     newDataUser
 });
 
+const updateDataCurrentProfileFailed = () => ({
+    type: actionTypes.UPDATE_DATA_CURRENT_PROFILE_FAILED,
+});
+
 export const updateDataCurrentProfile = newDataUser => dispatch => {
-    console.log(newDataUser);
-    debugger;
+    dispatch( updateDataCurrentProfileInit() )
+
     axios({
         method: 'put',
         url: '/profile',
@@ -43,11 +61,12 @@ export const updateDataCurrentProfile = newDataUser => dispatch => {
 
     })
     .then(response => {
-        console.log(response);
+
         dispatch( updateDataCurrentProfileSuccess(newDataUser) )
 
     })
     .catch(err => {
         console.log(err);
+        dispatch( updateDataCurrentProfileFailed() )
     })
 };
--- src/state/reducer/profile.js
+++ src/state/reducer/profile.js
@@ -1,12 +1,31 @@
 import * as actionTypes from '../action/actionTypes';
 
 const initialState = {
-    userData : {}
+    userData : {},
+    isError: false,
+    loading: true
 }
+
+const getDataCurrentProfileInit = (state, action) => ({
+    ...state,
+    loading: true,
+});
 
 const getDataCurrentProfileSuccess = (state, action) => ({
     ...state,
-    userData: action.userData
+    userData: action.userData,
+    loading: false,
+});
+
+const getDataCurrentProfileFailed = (state, action) => ({
+    ...state,
+    isError: true,
+    loading: false,
+});
+
+const updateDataCurrentProfileInit = (state, action) => ({
+    ...state,
+    loading: true,
 });
 
 const updateDataCurrentProfileSuccess = (state, action) => ({
@@ -14,14 +33,27 @@ const updateDataCurrentProfileSuccess = (state, action) => ({
     userData: {
         ...state.userData,
         ...action.newDataUser
-    }
+    },
+    loading: false,
+    isError: false,
+});
+
+const updateDataCurrentProfileFailed = (state, action) => ({
+    ...state,
+    isError: true,
+    loading: false,
+    
 });
 
 const reducer = (state = initialState, action) => {
     switch (action.type) {
+        case actionTypes.GET_DATA_CURRENT_PROFILE_INIT : return getDataCurrentProfileInit(state, action);
         case actionTypes.GET_DATA_CURRENT_PROFILE_SUCCESS : return getDataCurrentProfileSuccess(state, action);
-
+        case actionTypes.GET_DATA_CURRENT_PROFILE_FAILED : return getDataCurrentProfileFailed(state, action);
+        
+        case actionTypes.UPDATE_DATA_CURRENT_PROFILE_INIT : return updateDataCurrentProfileInit(state, action);
         case actionTypes.UPDATE_DATA_CURRENT_PROFILE_SUCCESS : return updateDataCurrentProfileSuccess(state, action);
+        case actionTypes.UPDATE_DATA_CURRENT_PROFILE_FAILED : return updateDataCurrentProfileFailed(state, action);
 
         default: return state;
     }
